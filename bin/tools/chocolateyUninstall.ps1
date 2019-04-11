2b4cbf977519b4c8997e8a677002b9bd253fe4fc
oleg.shilo <oleg.shilo@leicabiosystems.com>
2019-04-11 19:16:34 +1000

    - Addressed Choco moderation comments

--- bin/tools/chocolateyUninstall.ps1
+++ bin/tools/chocolateyUninstall.ps1
@@ -4,4 +4,4 @@ $installDir = Split-Path -Parent $MyInvocation.MyCommand.Definition
 $app = Join-Path $installDir "shell-x.exe"
 
 # Need to execute exe to unregister server.
-Start-Process -FilePath "$app" -ArgumentList "-u"
+Start-ChocolateyProcessAsAdmin -Statements "-u" -ExeToRun $app
