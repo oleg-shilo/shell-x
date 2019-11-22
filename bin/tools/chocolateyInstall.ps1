4743af71f222887d1e13787204a3a2527cd121c5
oleg.shilo <oleg.shilo@leicabiosystems.com>
2019-11-22 23:45:31 +1100

    Published release v1.0.1.0

--- bin/tools/chocolateyInstall.ps1
+++ bin/tools/chocolateyInstall.ps1
@@ -1,11 +1,11 @@
 $packageName = 'shell-x'
-$url = 'https://github.com/oleg-shilo/shell-x/releases/download/v1.0.0.0/shell-x.7z'
+$url = 'https://github.com/oleg-shilo/shell-x/releases/download/v1.0.1.0/shell-x.7z'
 
 
 $installDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $app = Join-Path $installDir "shell-x.exe"
 #Write-Host "$app"
-$cheksum = '16AE4B3E62EBE6AB46EF2FAB8E20F8EAF6B8653BC50A20D1A7EF648222B30B46'
+$cheksum = 'E53EBAAB8FDA245BCEEE7761D6117648CB0BA806FF080189C7BCC2FCC4C14C60'
 $checksumType = "sha256"
 
 # Download and unpack a zip file
@@ -15,5 +15,7 @@ Install-ChocolateyZipPackage "$packageName" "$url" "$installDir" -checksum $chec
 # Need to execute exe twice with special arguments in order configure the host system appropriately.
 # Register server and generate initial config
 
+
 Start-ChocolateyProcessAsAdmin -Statements "-r" -ExeToRun $app
-Start-ChocolateyProcessAsAdmin -Statements "-init -noui" -ExeToRun $app
+Start-ChocolateyProcessAsAdmin -Statements "-init -nogui" -ExeToRun $app
+
