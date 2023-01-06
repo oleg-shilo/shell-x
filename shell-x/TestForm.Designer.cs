84c05231d56333b802313972fbceadefd620196d
oleg.shilo <oleg.shilo@gmail.com>
2023-01-06 12:26:33 +1100

    Release v1.5.2 - Issue #12: Icon too big on nested context menu

---
+++ shell-x/TestForm.Designer.cs
@@ -0,0 +1,153 @@
+﻿namespace ShellX
+{
+    partial class TestForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.button1 = new System.Windows.Forms.Button();
+            this.pathTextBox = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.selectionCount = new System.Windows.Forms.NumericUpDown();
+            this.label2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.selectionCount)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 61);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(105, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Popup Menu";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // pathTextBox
+            // 
+            this.pathTextBox.Location = new System.Drawing.Point(13, 29);
+            this.pathTextBox.Name = "pathTextBox";
+            this.pathTextBox.Size = new System.Drawing.Size(256, 20);
+            this.pathTextBox.TabIndex = 2;
+            this.pathTextBox.TextChanged += new System.EventHandler(this.pathTextBox_TextChanged);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(123, 61);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(104, 23);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Init Config";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(233, 61);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(104, 23);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "Open Config";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 10);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(74, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Selected Path";
+            // 
+            // selectionCount
+            // 
+            this.selectionCount.Location = new System.Drawing.Point(293, 29);
+            this.selectionCount.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.selectionCount.Name = "selectionCount";
+            this.selectionCount.Size = new System.Drawing.Size(42, 20);
+            this.selectionCount.TabIndex = 6;
+            this.selectionCount.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(275, 31);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(12, 13);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "x";
+            // 
+            // TestForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(347, 93);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.selectionCount);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.pathTextBox);
+            this.Controls.Add(this.button1);
+            this.Name = "TestForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+            this.Text = "TestForm";
+            ((System.ComponentModel.ISupportInitialize)(this.selectionCount)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox pathTextBox;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.NumericUpDown selectionCount;
+        private System.Windows.Forms.Label label2;
+    }
+}
---
+++ shell-x/TestForm.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ShellX
+{
+    public partial class TestForm : Form
+    {
+        public static void Show(string path)
+        {
+            new TestForm() { initialPath = path }.ShowDialog();
+        }
+
+        public TestForm()
+        {
+            InitializeComponent();
+            this.VisibleChanged += (s, e)
+                => pathTextBox.Text = initialPath ?? Environment.CurrentDirectory;
+        }
+
+        string initialPath;
+
+        void Popup()
+        {
+            var selection = new List<string>();
+
+            for (int i = 0; i < selectionCount.Value; i++)
+                selection.Add(pathTextBox.Text);
+
+            var explorerSelction = new ExplorerSelectionStub(selection.ToArray());
+
+            if (!explorerSelction.CanShowMenu())
+            {
+                MessageBox.Show("No menu is configured for the specified path");
+                return;
+            }
+
+            var cm = explorerSelction.CreateMenu();
+
+            cm.Items.Add(new ToolStripSeparator());
+
+            var screen = Screen.FromPoint(Cursor.Position);
+            var left = screen.Bounds.X + screen.Bounds.Width / 2 - 200;
+            var top = screen.Bounds.Y + 150;
+            cm.Show(new Point(left, top));
+
+            cm.Focus();
+        }
+
+        void button1_Click(object sender, EventArgs e) => Popup();
+
+        void button2_Click(object sender, EventArgs e)
+        {
+            if (Directory.Exists(App.ConfigDir) && Directory.GetDirectories(App.ConfigDir).Any())
+                MessageBox.Show("Directory already exists and it is not empty.");
+            else
+                App.Main(new[] { "-init" });
+        }
+
+        void button3_Click(object sender, EventArgs e)
+        {
+            App.Main(new[] { "-open" });
+        }
+
+        private void pathTextBox_TextChanged(object sender, EventArgs e)
+        {
+        }
+    }
+}
