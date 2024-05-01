638eb016294de632237b74a7684412765280ecc8
Oleg Shilo <oleg.shilo@gmail.com>
2024-05-01 22:49:29 +1000

    Release v1.5.6 published on Choco

---
+++ bin/publish.cmd
@@ -0,0 +1,5 @@
+echo off
+rem choco apikey --key ???????? --source https://push.chocolatey.org/
+
+choco push shell-x.1.5.6.nupkg --source https://push.chocolatey.org/
+pause
--- bin/tools/chocolateyInstall.ps1
+++ bin/tools/chocolateyInstall.ps1
@@ -1,11 +1,11 @@
 $packageName = 'shell-x'
-$url = 'https://github.com/oleg-shilo/shell-x/releases/download/v1.5.5.0/shell-x.v1.5.5.0.7z'
+$url = 'https://github.com/oleg-shilo/shell-x/releases/download/v1.5.6.0/shell-x.v1.5.6.0.7z'
 
 
 $installDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $app = Join-Path $installDir "shell-x.exe"
 #Write-Host "$app"
-$cheksum = '19541FDB666B3EE38F8342CB77A90FD98512ED47A81358F99E66E613B29BE9AD'
+$cheksum = 'AD775CC5872D5CD4A475C5D77530E1713B32C0DDEA5C6DACDED34329DDCC873E'
 $checksumType = "sha256"
 
 # Download and unpack a zip file
