5895ccc1bda788d17fc4e4426e8dacfcf03e720c
Oleg Shilo <oleg.shilo@gmail.com>
2025-01-03 16:53:54 +1100

    Published on Chocolatey (v1.5.9.0)

--- bin/tools/chocolateyInstall.ps1
+++ bin/tools/chocolateyInstall.ps1
@@ -1,11 +1,11 @@
 $packageName = 'shell-x'
-$url = 'https://github.com/oleg-shilo/shell-x/releases/download/v1.5.7.0/shell-x.v1.5.7.0.7z'
+$url = 'https://github.com/oleg-shilo/shell-x/releases/download/v1.5.9.0/shell-x.v1.5.9.0.7z'
 
 
 $installDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $app = Join-Path $installDir "shell-x.exe"
 #Write-Host "$app"
-$checksum = 'AD775CC5872D5CD4A475C5D77530E1713B32C0DDEA5C6DACDED34329DDCC873E'
+$checksum = '773C3BD56258D96E831CB7429BC21267405B5D201A873F38E52B262EBBBC839D'
 $checksumType = "sha256"
 
 # Download and unpack a zip file
