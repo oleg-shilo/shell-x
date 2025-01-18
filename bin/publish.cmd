2a8430452c5297b6164a8e8e5853bda4a8057fd7
Oleg Shilo <oleg.shilo@gmail.com>
2025-01-18 11:39:36 +1100

    Published on choco

--- bin/publish.cmd
+++ bin/publish.cmd
@@ -1,5 +1,5 @@
 echo off
 rem choco apikey --key ???????? --source https://push.chocolatey.org/
 
-choco push shell-x.1.5.7.nupkg --source https://push.chocolatey.org/
+choco push shell-x.1.5.9.nupkg --source https://push.chocolatey.org/
 pause
